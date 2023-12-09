b9b51cd4b40990d9c2e6309299d5bdcd365b2f12
Melissa Flores <106169198+Meli357@users.noreply.github.com>
2023-12-09 12:25:54 +0100

    add sql server project to solution

---
+++ Database/dbo/Tables/Cities.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Cities] (
+    [id]   INT           IDENTITY (1, 1) NOT NULL,
+    [City] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Cities] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
