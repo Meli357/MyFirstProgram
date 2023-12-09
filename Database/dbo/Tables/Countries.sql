355a5d06230a3dc73e91a945f13c7694d8259a46
Melissa Flores <106169198+Meli357@users.noreply.github.com>
2023-12-09 13:21:29 +0100

    add new table to database

---
+++ Database/dbo/Tables/Countries.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Countries] (
+    [id]      INT           IDENTITY (1, 1) NOT NULL,
+    [Country] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Country] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
